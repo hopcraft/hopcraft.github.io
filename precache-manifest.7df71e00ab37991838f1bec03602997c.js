afee38bfeb85080b1f09862d999deea0a3f1776e
hopcraft@foxmail.com <zhaoheping@meizhitec.com>
2019-09-19 18:55:27 +0800

    采用 React 重写首页

---
+++ precache-manifest.7df71e00ab37991838f1bec03602997c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c43a4bcabcaab4b679b1728ed09da50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f051287aec4356c58a68",
+    "url": "/static/css/main.d9517c1a.chunk.css"
+  },
+  {
+    "revision": "1950cfaefa44662c9761",
+    "url": "/static/js/2.51ed8af5.chunk.js"
+  },
+  {
+    "revision": "f051287aec4356c58a68",
+    "url": "/static/js/main.dec2680c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eef2d8adf052c86bbd509b0fcf44f880",
+    "url": "/static/media/background.eef2d8ad.png"
+  }
+]);
